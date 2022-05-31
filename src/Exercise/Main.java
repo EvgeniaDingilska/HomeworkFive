7b042e5c00440bdae3f7ca6cc95eaea0546814a7
Evgenia Dingilska <61237972+EvgeniaDingilska@users.noreply.github.com>
2022-05-31 19:15:03 +0300

    Update Main.java

--- src/Exercise/Main.java
+++ src/Exercise/Main.java
@@ -2,6 +2,6 @@ package Exercise;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world and Eva!");
+        System.out.println("Hello world and Eva !");
     }
 }
